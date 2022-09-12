0689d1b3772a64e85b845dbdc9a5fbbe027e8da6
vinraelll <53097647+vinraelll@users.noreply.github.com>
2022-09-12 15:38:49 +0300

    included global .scss files, such as reset styles, mixins etc.

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import * as flsFunctions from './modules/functions.js'
 
 flsFunctions.testWebP()
 
+// included swiper as default because it's commonly often used, but if you don't need, you can always delete it
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper()
